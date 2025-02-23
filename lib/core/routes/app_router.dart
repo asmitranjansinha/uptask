4a453e584b917ac0d342bbd14f4d05936036519a
asmy8h <sinhaasmit99@gmail.com>
2025-02-23 22:02:24 +0530

    :sparkles: added filter options :lipstick: added sliver app bar in task list view

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -3,8 +3,6 @@ import 'package:go_router/go_router.dart';
 import 'package:uptask/features/auth/presentation/pages/login_page.dart';
 import 'package:uptask/features/auth/presentation/pages/register_page.dart';
 import 'package:uptask/features/splash/splash_page.dart';
-import 'package:uptask/features/task/presentation/pages/task_form_page.dart';
-import 'package:uptask/features/task/presentation/pages/task_home_page.dart';
 import 'package:uptask/features/task/presentation/pages/task_list_page.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
--- lib/features/task/presentation/pages/task_list_page.dart
+++ lib/features/task/presentation/pages/task_list_page.dart
@@ -10,8 +10,21 @@ import 'package:uptask/features/task/presentation/bloc/task_state.dart';
 import 'package:uptask/features/task/presentation/widgets/task_create_bottom_sheet.dart';
 import 'package:uptask/features/task/presentation/widgets/task_item.dart';
 
-class TaskListPage extends StatelessWidget {
+class TaskListPage extends StatefulWidget {
   const TaskListPage({super.key});
+
+  @override
+  State<TaskListPage> createState() => _TaskListPageState();
+}
+
+class _TaskListPageState extends State<TaskListPage> {
+  String _selectedFilter = 'All';
+
+  void _selectFilter(String filter) {
+    setState(() {
+      _selectedFilter = filter;
+    });
+  }
 
   void _showTaskCreateBottomSheet(BuildContext context) {
     showModalBottomSheet(
@@ -27,71 +40,132 @@ class TaskListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Tasks')),
-      body: BlocBuilder<TaskBloc, TaskState>(
-        builder: (context, state) {
-          if (state is TaskLoaded || state is TaskLoadingWithData) {
-            final tasks = state is TaskLoaded
-                ? state.tasks
-                : (state as TaskLoadingWithData).tasks;
-
-            final groupedTasks = _groupTasksByDate(tasks);
-
-            return ListView(
-              children: _getSortedKeys(groupedTasks).map((key) {
-                return Column(
+      body: Padding(
+        padding: EdgeInsets.only(top: 30.sp),
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Colors.white,
+              expandedHeight: 100.h,
+              floating: false,
+              surfaceTintColor: Colors.white,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
-                      padding: EdgeInsets.symmetric(
-                          vertical: 4.sp, horizontal: 16.0),
-                      child: Text(
-                        key,
-                        style: GoogleFonts.fredoka(
-                          fontSize: 18.sp,
-                          fontWeight: FontWeight.w500,
-                        ),
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.primary,
+                              borderRadius: BorderRadius.circular(10.sp),
+                            ),
+                            padding: EdgeInsets.all(5.sp),
+                            child: Icon(
+                              CupertinoIcons.list_bullet_indent,
+                              color: Colors.white,
+                              size: 28.sp,
+                            ),
+                          ),
+                          8.horizontalSpace,
+                          Text(
+                            'Tasks',
+                            style: GoogleFonts.fredoka(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    ...groupedTasks[key]!.map((task) => TaskItem(task: task)),
                   ],
-                );
-              }).toList(),
-            );
-          } else if (state is TaskFailure) {
-            return Center(child: Text(state.error));
-          }
-          return Center(
-            child: InkWell(
-              onTap: () => _showTaskCreateBottomSheet(context),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Start Creating Tasks',
-                    style: GoogleFonts.fredoka(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w400,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                  10.horizontalSpace,
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 143, 124, 237),
-                      shape: BoxShape.circle,
-                    ),
-                    child: const Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+                ),
+              ),
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(15.h),
+                child: _buildFilterOptions(),
               ),
             ),
-          );
-        },
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return BlocBuilder<TaskBloc, TaskState>(
+                    builder: (context, state) {
+                      if (state is TaskLoaded || state is TaskLoadingWithData) {
+                        final tasks = state is TaskLoaded
+                            ? state.tasks
+                            : (state as TaskLoadingWithData).tasks;
+
+                        final filteredTasks = _filterTasks(tasks);
+                        final groupedTasks = _groupTasksByDate(filteredTasks);
+
+                        return Column(
+                          children: _getSortedKeys(groupedTasks).map((key) {
+                            return Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 4.sp, horizontal: 16.0),
+                                  child: Text(
+                                    key,
+                                    style: GoogleFonts.fredoka(
+                                      fontSize: 18.sp,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                                ...groupedTasks[key]!.map(
+                                  (task) => TaskItem(task: task),
+                                ),
+                              ],
+                            );
+                          }).toList(),
+                        );
+                      } else if (state is TaskFailure) {
+                        return Center(child: Text(state.error));
+                      }
+                      return Center(
+                        child: InkWell(
+                          onTap: () => _showTaskCreateBottomSheet(context),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Start Creating Tasks',
+                                style: GoogleFonts.fredoka(
+                                  fontSize: 14.sp,
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                              10.horizontalSpace,
+                              Container(
+                                decoration: const BoxDecoration(
+                                  color: Color.fromARGB(255, 143, 124, 237),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: const Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                childCount: 1,
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: BlocBuilder<TaskBloc, TaskState>(
         builder: (context, state) {
@@ -109,6 +183,39 @@ class TaskListPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  Widget _buildFilterOptions() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildFilterTile('All'),
+          _buildFilterTile('Complete'),
+          _buildFilterTile('Low'),
+          _buildFilterTile('Medium'),
+          _buildFilterTile('High'),
+        ],
+      ),
+    );
+  }
+
+  List<TaskEntity> _filterTasks(List<TaskEntity> tasks) {
+    switch (_selectedFilter) {
+      case 'All':
+        return tasks;
+      case 'Complete':
+        return tasks.where((task) => task.isCompleted).toList();
+      case 'Low':
+        return tasks.where((task) => task.priority == 'Low').toList();
+      case 'Medium':
+        return tasks.where((task) => task.priority == 'Medium').toList();
+      case 'High':
+        return tasks.where((task) => task.priority == 'High').toList();
+      default:
+        return tasks;
+    }
   }
 
   Map<String, List<TaskEntity>> _groupTasksByDate(List<TaskEntity> tasks) {
@@ -154,5 +261,45 @@ class TaskListPage extends StatelessWidget {
     });
 
     return keys;
+  }
+
+  Widget _buildFilterTile(String filter) {
+    bool isSelected = _selectedFilter == filter;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.sp),
+      child: InkWell(
+        onTap: () => _selectFilter(filter),
+        child: Container(
+          decoration: BoxDecoration(
+            color: isSelected ? Colors.black : Colors.white,
+            border: Border.all(
+              color: Colors.black,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(35.sp),
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: 10.sp,
+            vertical: 5.sp,
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                filter,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.fredoka(
+                  fontSize: 12.sp,
+                  fontWeight: FontWeight.w500,
+                  color: isSelected ? Colors.white : Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
