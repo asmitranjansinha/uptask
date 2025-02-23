ad29b56018760d2bb172a4107eb13cf85cb2d910
asmy8h <sinhaasmit99@gmail.com>
2025-02-23 16:25:34 +0530

    :zap: modified app text styles fontsize to follow size util

--- lib/core/constants/app_text_styles.dart
+++ lib/core/constants/app_text_styles.dart
@@ -1,91 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'app_colors.dart';
 
 class AppTextStyles {
   static final TextStyle displayLarge = GoogleFonts.fredoka(
-    fontSize: 36,
+    fontSize: 36.sp,
     fontWeight: FontWeight.bold,
     color: AppColors.text,
   );
 
   static final TextStyle displayMedium = GoogleFonts.fredoka(
-    fontSize: 32,
+    fontSize: 32.sp,
     fontWeight: FontWeight.bold,
     color: AppColors.text,
   );
 
   static final TextStyle displaySmall = GoogleFonts.fredoka(
-    fontSize: 28,
+    fontSize: 28.sp,
     fontWeight: FontWeight.bold,
     color: AppColors.text,
   );
 
   static final TextStyle headlineLarge = GoogleFonts.fredoka(
-    fontSize: 24,
+    fontSize: 24.sp,
     fontWeight: FontWeight.bold,
     color: AppColors.text,
   );
 
   static final TextStyle headlineMedium = GoogleFonts.fredoka(
-    fontSize: 22,
+    fontSize: 22.sp,
     fontWeight: FontWeight.bold,
     color: AppColors.text,
   );
 
   static final TextStyle headlineSmall = GoogleFonts.fredoka(
-    fontSize: 20,
+    fontSize: 20.sp,
     fontWeight: FontWeight.bold,
     color: AppColors.text,
   );
 
   static final TextStyle titleLarge = GoogleFonts.fredoka(
-    fontSize: 18,
+    fontSize: 18.sp,
     fontWeight: FontWeight.bold,
     color: AppColors.text,
   );
 
   static final TextStyle titleMedium = GoogleFonts.fredoka(
-    fontSize: 16,
+    fontSize: 16.sp,
     fontWeight: FontWeight.w600,
     color: AppColors.text,
   );
 
   static final TextStyle titleSmall = GoogleFonts.fredoka(
-    fontSize: 14,
+    fontSize: 14.sp,
     fontWeight: FontWeight.w600,
     color: AppColors.text,
   );
 
   static final TextStyle bodyLarge = GoogleFonts.fredoka(
-    fontSize: 16,
+    fontSize: 16.sp,
     color: AppColors.text,
   );
 
   static final TextStyle bodyMedium = GoogleFonts.poppins(
-    fontSize: 14,
+    fontSize: 14.sp,
     color: AppColors.text,
   );
 
   static final TextStyle bodySmall = GoogleFonts.poppins(
-    fontSize: 12,
+    fontSize: 12.sp,
     color: AppColors.text,
   );
 
   static final TextStyle labelLarge = GoogleFonts.fredoka(
-    fontSize: 14,
+    fontSize: 14.sp,
     fontWeight: FontWeight.w500,
     color: AppColors.buttonText,
   );
 
   static final TextStyle labelMedium = GoogleFonts.fredoka(
-    fontSize: 12,
+    fontSize: 12.sp,
     fontWeight: FontWeight.w500,
     color: AppColors.buttonText,
   );
 
   static final TextStyle labelSmall = GoogleFonts.fredoka(
-    fontSize: 10,
+    fontSize: 10.sp,
     fontWeight: FontWeight.w500,
     color: AppColors.buttonText,
   );
